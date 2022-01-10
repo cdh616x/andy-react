637f4eff79039e2aa8b75aa290916bd0957543ce
cdh616x <91150236+cdh616x@users.noreply.github.com>
2022-01-09 23:20:55 -0500

    new build
    added instagram icon to footer component and fontawesome script to bottom of index.html

--- andy/src/components/Footer.jsx
+++ andy/src/components/Footer.jsx
@@ -4,10 +4,17 @@ import React from "react";
 import FooterImg from "../full-images/banner_text.png";
 
 export default function Footer() {
-  let year = new Date().getYear()
+  let year = new Date().getYear();
   return (
     <div className="text-center">
+    <div className="row">
     <img className="footer mx-auto" src={FooterImg} />
+    </div>
+    <h2>
+    <a href="#">
+    <i class="fab fa-instagram-square"></i>
+    </a>
+    </h2>
     <h5 className="copyright">Andy Shaw © {year + 1900}</h5>
     </div>
   );
