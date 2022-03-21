9f8f9f5f87baebb06485daa83a8395534fd6d07b
cdh616x <91150236+cdh616x@users.noreply.github.com>
2022-03-21 13:25:44 -0400

    changed homepage in package.json and configured to send to new domain

--- andy/src/components/About.jsx
+++ andy/src/components/About.jsx
@@ -8,7 +8,8 @@ export default function About() {
     <>
     <div className="row about">
     <div className="col-lg-6 col-sm-6">
-    <p className="about-me">Andy Shaw is a freelance artist/illustrator based out of Grand Rapids, Michigan, specializing in digital paintings and pen and ink drawings packed with detail and tight linework. His indefatigable interest in fantasy and science fiction sprouts hosts of stalwart knights, intrepid spacefarers, and landscapes of gnarled roots and billowing clouds.</p>
+    <p className="about-me">Andy Shaw is a freelance artist/illustrator based out of Grand Rapids, Michigan, specializing in digital paintings and pen and ink drawings packed with detail and tight linework.</p>
+    <p className="about-me">His indefatigable interest in fantasy and science fiction sprouts hosts of stalwart knights, intrepid spacefarers, and landscapes of gnarled roots and billowing clouds.</p>
     </div>
     <div className="col-lg-6 col-sm-6">
     <h1>
